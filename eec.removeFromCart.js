6a6219502a1131d915a7ad8bb3986ecd543a2037
Carlos M. Díaz Honrado <carlos@carlosmdh.es>
2022-04-14 10:34:01 +0200

    Update eec.removeFromCart.js

--- eec.removeFromCart.js
+++ eec.removeFromCart.js
@@ -7,11 +7,11 @@ window.dataLayer.push({
         'remove': {                 // nombre de una acción. En este caso SIEMPRE es remove
         'products': [{              // lista de productos eliminados a un carrito. Puede contener varios productos al mismo tiempo.
             'name': 'Citizen - SUPER TITANIUM. MODEL: AW0060-11P',      // nombre del productos que se eliminó del carrito
-            'id': 'CIT30283',       // id del producto
-            'price': '330.00',      // precio del producto
-            'brand': 'Citizen',     // marca del producto
-            'category': 'Watches',  // categoría del producto
-            'variant': 'Silver'     // variante de producto Si no hay variantes, excluye esta clave de dataLayer.push
+            'id': 'CIT30283',                                           // id del producto
+            'price': '330.00',                                          // precio del producto
+            'brand': 'Citizen',                                         // marca del producto
+            'category': 'Watches',                                      // categoría del producto
+            'variant': 'Silver'                                         // variante de producto Si no hay variantes, excluye esta clave de dataLayer.push
         }]
         }
     }
