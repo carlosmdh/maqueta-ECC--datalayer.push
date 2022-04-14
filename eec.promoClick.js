e7c9591acf7527adf7b4282b36b471b3c243e7a6
Carlos M. Díaz Honrado <carlos@carlosmdh.es>
2022-04-14 10:20:31 +0200

    Update eec.promoClick.js

--- eec.promoClick.js
+++ eec.promoClick.js
@@ -1,15 +1,15 @@
 <script>
 window.dataLayer  = window.dataLayer || [];
 window.dataLayer.push({
-    'event': 'eec.promoClick',                  // name of an event. In this case, always stays as eec.promoClick
-    'ecommerce': {                              // ecommerce object. This is the essential part of tracking. New data must always be pushed to this object
-        'promoClick': {                         // name of an action. In this case, always stays as promoClick
-          'promotions': [                       // list of banners that a user has clicked. usually it contains only one promotion
+    'event': 'eec.promoClick',                  // nombre del evento. En este caso SIEMPRE es eec.promoClick
+    'ecommerce': {                              // objeto ecommerce. Esta es la parte esencial del seguimiento. Los nuevos datos de EE siempre deben enviarse a este objeto
+        'promoClick': {                         // nombre de la acción. En este caso, SIEMPRE es promoClick
+          'promotions': [                       // lista de banners en los que un usuario ha hecho clic. por lo general contiene una sola promoción
            {
-             'id': 'summer_sale2019_1',         // id of a banner         
-             'name': 'Summer sale 2019_1',      // name of a banner
-             'creative': 'Home Page Carousel',  // type of a banner 
-             'position': 'Slide 1'              // position of a banner
+             'id': 'summer_sale2019_1',         // id del banner         
+             'name': 'Summer sale 2019_1',      // nombre del banner
+             'creative': 'Home Page Carousel',  // tipo de banner 
+             'position': 'Slide 1'              // posición del banner
             }]
         }
   }
