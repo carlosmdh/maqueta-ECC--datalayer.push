0a533e6cfce269a4b9ecd0b5390726ca7740c641
Carlos M. Díaz Honrado <carlos@carlosmdh.es>
2022-04-14 10:18:41 +0200

    Update eec.productImpression.js

--- eec.productImpression.js
+++ eec.productImpression.js
@@ -1,29 +1,30 @@
 <script>
 window.dataLayer  = window.dataLayer || [];
 window.dataLayer.push({
-	'event': 'eec.productImpression',   	// name of an event. In this case, always stays as eec.productImpression
-	'ecommerce': {							// ecommerce object. This is the essential part of tracking. New EE data must always be pushed to this object
-		'currencyCode': 'EUR', 				// the currency which is currently set by a visitor                      
-		'impressions': [					// type of an ecommerce action. In this case, always stays as impressions
+	'event': 'eec.productImpression',   	// nombre del evento. En este caso SIEMPRE es eec.productImpression
+	'ecommerce': {							// objeto ecommerce. Esta es la parte esencial del seguimiento. Los nuevos datos de EE siempre deben enviarse a este objeto
+		'currencyCode': 'EUR', 				// la moneda que está configurada actualmente
+		'impressions': [					// tipo de una acción de comercio electrónico. En este caso, SIEMPRE es impressions
 		 {
-			'name': 'Citizen - SUPER TITANIUM. MODEL: AW0060-11P',      // name of a product that is displayed. Always include it.
-			'id': 'CIT30283',     			// id of a product. Always include it.
-			'price': '330.00',				// price of a product
-			'brand': 'Citizen',				// brand/vendor of a product
-			'category': 'Watches',			// category of a product
-			'variant': 'Silver',       		// product variant. If there are no variants, exclude this key from the dataLayer.push
-			'list': 'main',					// name of a product list. See all the possible list names in the specification
-			'position': '1'     			// number of a position (in which a product was visible at that time)
+			'name': 'Citizen - SUPER TITANIUM. MODEL: AW0060-11P',		// nombre del producto que se muestra. INCLUIRLO SIEMPRE
+			'id': 'CIT30283',						// id del producto. INCLUIRLO SIEMPRE
+			'price': '330.00',						// precio del producto
+			'brand': 'Citizen',						// marca del producto
+			'category': 'Watches',						// categoría producto
+			'variant': 'Silver',       					// Variación del producto. En caso de no existir variaciones, se puede excluir del dataLayer.pus
+			'list': 'main',							// nombre de una lista de productos. Incluir todos los nombres de lista posibles en la especificación
+			'position': '1'     						// número de la posición en la que un producto era visible en ese momento
 		},			
 		 {
-			'name': 'Citizen - BUSINESS CLASS. MODEL: BC0099-11Z',      
-			'id': 'CIT30288',    
-			'price': '290.00',
-			'brand': 'Citizen',
-			'category': 'Watches',
-			'variant': 'Silver',       
-			'list': 'main',
-			'position': '2'     }]
+			'name': 'Citizen - BUSINESS CLASS. MODEL: BC0099-11Z',		// nombre del producto que se muestra. INCLUIRLO SIEMPRE
+			'id': 'CIT30288',						// id del producto. INCLUIRLO SIEMPRE
+			'price': '290.00',						// precio del producto
+			'brand': 'Citizen',						// marca del producto
+			'category': 'Watches',						// categoría producto
+			'variant': 'Silver',						// Variación del producto. En caso de no existir variaciones, se puede excluir del dataLayer.pus
+			'list': 'main',							// nombre de una lista de productos. Incluir todos los nombres de lista posibles en la especificación
+			'position': '2'							// número de la posición en la que un producto era visible en ese momento
+		 }]
 	}
 });
 </script>
