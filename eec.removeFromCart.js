1c3b83db6f1c0c1d6583b98627e92dfc963a9c6d
Carlos M. Díaz Honrado <carlos@carlosmdh.es>
2022-04-14 10:29:12 +0200

    Update eec.removeFromCart.js

--- eec.removeFromCart.js
+++ eec.removeFromCart.js
@@ -1,17 +1,17 @@
 <script>
 window.dataLayer  = window.dataLayer || [];
 window.dataLayer.push({
-    'event': 'eec.removeFromCart',  // name of an event. In this case, always stays as eec.removeFromCart
-    'ecommerce': {                  // ecommerce object. This is the essential part of tracking. New EE data must always be pushed to this object
-        'currencyCode': 'EUR',      // the currency which is currently used by a visitor 
-        'remove': {                 // name of an action. In this case, always stays as remove                            
-        'products': [{              // list of products removed from a cart. can include multiple products.
-            'name': 'Citizen - SUPER TITANIUM. MODEL: AW0060-11P',      // name of a product that was removed from a cart
-            'id': 'CIT30283',       // id of a product
-            'price': '330.00',      // price of a product
-            'brand': 'Citizen',     // brand/vendor of a product
-            'category': 'Watches',  // category of a product
-            'variant': 'Silver'     // product variant. If there are no variants, exclude this key from the dataLayer.push
+    'event': 'eec.removeFromCart',  // nombre del evento. En este caso SIEMPRE es eec.removeFromCart
+    'ecommerce': {                  // objeto ecommerce. Esta es la parte esencial del seguimiento. Los nuevos datos de EE siempre deben enviarse a este objeto
+        'currencyCode': 'EUR',      // la moneda que utiliza actualmente un visitante
+        'remove': {                 // nombre de una acción. En este caso SIEMPRE es remove
+        'products': [{              // lista de productos eliminados a un carrito. Puede contener varios productos al mismo tiempo.
+            'name': 'Citizen - SUPER TITANIUM. MODEL: AW0060-11P',      // nombre del productos que se eliminó del carrito
+            'id': 'CIT30283',       // id del producto
+            'price': '330.00',      // precio del producto
+            'brand': 'Citizen',     // marca del producto
+            'category': 'Watches',  // categoría del producto
+            'variant': 'Silver'     // variante de producto Si no hay variantes, excluye esta clave de dataLayer.push
         }]
         }
     }
