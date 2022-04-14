0e310e586d9955c3842bcf4f3559c7e1362f7cc6
Carlos M. Díaz Honrado <carlos@carlosmdh.es>
2022-04-14 10:30:21 +0200

    Update eec.checkout-Step2.js

--- eec.checkout-Step2.js
+++ eec.checkout-Step2.js
@@ -2,7 +2,7 @@
 window.dataLayer = window.dataLayer || [];
 window.dataLayer.push({
     'event': 'eec.checkout',                // nombre del evento. En este caso SIEMPRE es eec.checkout
-    'ecommerce': {                          // objeto de comercio electrónico. Esta es la parte esencial del seguimiento. Los nuevos datos de EE siempre deben enviarse a este objeto
+    'ecommerce': {                          // objeto ecommerce. Esta es la parte esencial del seguimiento. Los nuevos datos de EE siempre deben enviarse a este objeto
         'checkout': {                       /// nombre de una acción. En este caso SIEMPRE es checkout
             'actionField': {    
                 'step': 2                   // número del paso de pago actual (siempre debe ser 2 en esta situación)
