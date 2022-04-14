42e650a3eba6bebc6c6d98332e45077179435ad1
Carlos M. Díaz Honrado <carlos@carlosmdh.es>
2022-04-14 10:09:47 +0200

    Update eec.productDetail.js

--- eec.productDetail.js
+++ eec.productDetail.js
@@ -1,17 +1,17 @@
 <script>
 window.dataLayer  = window.dataLayer || [];
 window.dataLayer.push({
-    'event': 'eec.productDetail',       // name of an event. In this case, always stays as eec.productDetail
-    'ecommerce': {                      // ecommerce object. This is the essential part of tracking. New EE data must always be pushed to this object
-        'detail': {                     // name of an action. In this case, always stays as detail
-          'actionField': {'list': 'main'},    // optional. if this event happened in a certain list, pass its name with this key
+    'event': 'eec.productDetail',       // nombre del evento. En este caso SIEMPRE es eec.productDetail
+    'ecommerce': {                      // objeto de comercio electrónico. Esta es la parte esencial del seguimiento. Los nuevos datos de EE siempre deben enviarse a este objeto
+        'detail': {                     // nombre d ela acción. En este caso SIEMPRE es detail
+          'actionField': {'list': 'main'},    // si este evento sucedió en una lista determinada, pase su nombre con esta clave. OPCIONAL
           'products': [{
-            'name': 'Citizen - SUPER TITANIUM. MODEL: AW0060-11P',      // name of a product that is currently viewed. Always include it.
-            'id': 'CIT30283',           // id of a product. Always include it.
-            'price': '330.00',          // price of a product
-            'brand': 'Citizen',         // brand/vendor of a product
-            'category': 'Watches',      // category of a product
-            'variant': 'Silver'         // product variant. If there are no variants, exclude this key from the dataLayer.push
+            'name': 'Citizen - SUPER TITANIUM. MODEL: AW0060-11P',      // nombre del producto que se está viendo actualmente. INCLUIRLO SIEMPRE
+            'id': 'CIT30283',           // id del producto. INCLUIRLO SIEMPRE
+            'price': '330.00',          // precio del producto
+            'brand': 'Citizen',         // marca del producto
+            'category': 'Watches',      // categoría del producto
+            'variant': 'Silver'         // Variación del producto. En caso de no existir variaciones, se puede excluir
            }]
          }
     }
