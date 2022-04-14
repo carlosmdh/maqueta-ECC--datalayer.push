81bc8c50b69b3fe26709c5502a5b68072f4d1df3
Carlos M. Díaz Honrado <carlos@carlosmdh.es>
2022-04-14 09:45:00 +0200

    Update eec.addToCart.js

--- eec.addToCart.js
+++ eec.addToCart.js
@@ -1,21 +1,21 @@
 <script>
 window.dataLayer  = window.dataLayer || [];
 window.dataLayer.push({
-    'event': 'eec.addToCart',       // name of an event. In this case, always stays as eec.addToCart
-    'ecommerce': {                  // ecommerce object. This is the essential part of tracking. New EE data must always be pushed to this object
-        'currencyCode': 'EUR',          // the currency which is currently used by a visitor 
-        'add': {                        // name of an action. In this case, always stays as add
+    'event': 'eec.addToCart',               // nombre del evento. En este caso SIEMPRE es eec.addToCart
+    'ecommerce': {                          // objeto de comercio electrónico. Esta es la parte esencial del seguimiento. Los nuevos datos de EE siempre deben enviarse a este objeto
+        'currencyCode': 'EUR',              // la moneda que utiliza actualmente un visitante
+        'add': {                            // nombre de una acción. En este caso SIEMPRE es como add
             'actionField': {            
-                'list': 'main'          // optional. name of a list from which the product was added to a cart  
+                'list': 'main'              // nombre de una lista desde la cual se agregó el producto a un carrito. OPCIONAL.
               },        
-            'products': [{              // list of products added to a cart. Can contain multiple products at the same time
-                'name': 'Citizen - SUPER TITANIUM. MODEL: AW0060-11P',      // name of a product that was added to a cart
-                'id': 'CIT30283',       // id of a product
-                'price': '330.00',      // price of a product
-                'brand': 'Citizen',     // brand/vendor of a product
-                'category': 'Watches',  // category of a product
-                'variant': 'Silver',     // product variant. If there are no variants, exclude this key from the dataLayer.push
-                'quantity': 1
+            'products': [{                  // lista de productos añadidos a un carrito. Puede contener varios productos al mismo tiempo.
+                'name': 'Citizen - SUPER TITANIUM. MODEL: AW0060-11P',      // nombre de un producto que se agregó a un carrito
+                'id': 'CIT30283',           // id del producto
+                'price': '330.00',          // Precio del producto
+                'brand': 'Citizen',         // marca del producto
+                'category': 'Watches',      // categoría del producto
+                'variant': 'Silver',        // variante de producto Si no hay variantes, excluye esta clave de dataLayer.push
+                'quantity': 1               // cantidad del producto agregado al carrito.
             }]
         }
     }
