068ea5361f0bcd73d1293df823749ef76b269820
Carlos M. Díaz Honrado <carlos@carlosmdh.es>
2022-04-14 10:29:18 +0200

    Update eec.addToCart.js

--- eec.addToCart.js
+++ eec.addToCart.js
@@ -2,14 +2,14 @@
 window.dataLayer  = window.dataLayer || [];
 window.dataLayer.push({
     'event': 'eec.addToCart',               // nombre del evento. En este caso SIEMPRE es eec.addToCart
-    'ecommerce': {                          // objeto de comercio electrónico. Esta es la parte esencial del seguimiento. Los nuevos datos de EE siempre deben enviarse a este objeto
+    'ecommerce': {                          // objeto ecommerce. Esta es la parte esencial del seguimiento. Los nuevos datos de EE siempre deben enviarse a este objeto
         'currencyCode': 'EUR',              // la moneda que utiliza actualmente un visitante
         'add': {                            // nombre de una acción. En este caso SIEMPRE es add
             'actionField': {            
                 'list': 'main'              // nombre de una lista desde la cual se agregó el producto a un carrito. OPCIONAL.
               },        
             'products': [{                  // lista de productos añadidos a un carrito. Puede contener varios productos al mismo tiempo.
-                'name': 'Citizen - SUPER TITANIUM. MODEL: AW0060-11P',      // nombre de un producto que se agregó a un carrito
+                'name': 'Citizen - SUPER TITANIUM. MODEL: AW0060-11P',      // nombre del producto que se agregó al carrito
                 'id': 'CIT30283',           // id del producto
                 'price': '330.00',          // Precio del producto
                 'brand': 'Citizen',         // marca del producto
